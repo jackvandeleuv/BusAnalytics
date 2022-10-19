ace4703954ec59bf138474d54d99249cf33d4acc
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2022-10-19 17:09:20 -0500

    Reformatted scrape_truetime

--- scrape_truetime.py
+++ scrape_truetime.py
@@ -18,8 +18,10 @@ class Scraper:
 
     # This function checks the HTML page for a particular bus route, and determines whether the route includes INBOUND
     # buses, OUTBOUND buses, both, or neither. The result is two booleans values: outbound and inbound.
-    def __check_available_directions(route_num):
-        url = f'https://truetime.portauthority.org/bustime/wireless/html/selectdirection.jsp?route=Port%20Authority%20Bus:{route_num}'
+    @staticmethod
+    def check_available_directions(route_num):
+        url = f'https://truetime.portauthority.org/bustime/wireless/html/selectdirection' \
+              f'.jsp?route=Port%20Authority%20Bus:{route_num}'
         page = requests.get(url)
         # Specify lxml parser to avoid different defaults on different machines
         soup = bs4.BeautifulSoup(page.text, features='lxml')
@@ -33,9 +35,10 @@ class Scraper:
 
         return outbound, inbound
 
-    # This function takes in a url and HTML tag, and then scrapes the url for the given tag. It returns a list of strings,
-    # which contains every string found inside the given tag at the given url.
-    def __scrape_html_tag(self, url, tag):
+    # This function takes in a url and HTML tag, and then scrapes the url for the given tag. It returns a list of
+    # strings, which contains every string found inside the given tag at the given url.
+    @staticmethod
+    def scrape_html_tag(url, tag):
         page = requests.get(url)
         # Specify lxml parser to avoid different default parsers on different machines
         soup_object = bs4.BeautifulSoup(page.text, features='lxml')
@@ -46,12 +49,13 @@ class Scraper:
             result.append(element.string)
         return result
 
-
-    # This function takes in a url, which should be the page on the TrueTime website which lists all the stops available for
-    # a given route and direction. The function also takes in information about direction, route ID, route name. Using the
-    # given URL, the function scrapes the ID and name of each bus stop, and packages it together with the other parameters
-    # as a dict with the following structure for each key/value pair: {StopID: [StopName, Direction, RouteID, RouteName]}
-    def __zip_stop_id_and_name(self, url, direction, r_num, r_name):
+    # This function takes in a url, which should be the page on the TrueTime website which lists all the stops
+    # available for a given route and direction. The function also takes in information about direction, route ID, route
+    # name. Using the given URL, the function scrapes the ID and name of each bus stop, and packages it together with
+    # the other parameters as a dict with the following structure for each key/value pair: {StopID: [StopName,
+    # Direction, RouteID, RouteName]}
+    @staticmethod
+    def zip_stop_id_and_name(url, direction, r_num, r_name):
         page = requests.get(url)
         # Specify lxml parser to avoid different default parsers on different machines
         soup_object = bs4.BeautifulSoup(page.text, features='lxml')
@@ -64,8 +68,8 @@ class Scraper:
             link_address = link.get('href')
             # Strip off the end of the link using regex.
             end_of_link = re.search(r'id=Port\+Authority\+Bus%3A[0-9]+', link_address).group()
-            # Get the stop_id from the end_of_link str using regex. Exploit the fact that the str we want always starts with
-            # A.
+            # Get the stop_id from the end_of_link str using regex. Exploit the fact that the str we want always starts
+            # with A.
             stop_id = re.search(r'A[0-9]+', end_of_link).group()
             # Strip off the A.
             stop_id = stop_id[1:]
@@ -82,9 +86,11 @@ class Scraper:
 
         return stop_info
 
-    # This function take a URL and an HTML class name as input, and outputs a list of strings containing all the information
-    # found under the given url and class tag. Unlike bs4, Selenium works for content served dynamically by JavaScript.
-    def __scrape_dynamic_tag(self, url, class_name):
+    # This function take a URL and an HTML class name as input, and outputs a list of strings containing all the
+    # information found under the given url and class tag. Unlike bs4, Selenium works for content served dynamically
+    # by JavaScript.
+    @staticmethod
+    def scrape_dynamic_tag(url, class_name):
         # This is boilerplate code that initializes a Chrome web driver for use by the Selenium library.
         options = Options()
         # Stop browser windows from actually popping up.
@@ -106,17 +112,18 @@ class Scraper:
 
     # This is the main function for this module. It takes no arguments, and outputs a list of dicts. There is a dict for
     # each line/direction combination (e.g. 71A OUTBOUND). Each key in each dict represents a unique bus stop/bus route
-    # combination. Most bus stops have multiple lines that stop there, so between the different dicts there are identical
-    # keys.
+    # combination. Most bus stops have multiple lines that stop there, so between the different dicts there are
+    # identical keys.
 
     # Input: None
-    # Output: get_stop_list() returns a list of dicts. There are 0-2 dicts per bus route, depending on whether INBOUND and
-    #         OUTBOUND versions both exist for that route. Each dict has the following structure:
-    #         {StopIDNumber: [StopName, Direction, RouteID, RouteName]
-    def __get_stop_list(self):
+    # Output: get_stop_list() returns a list of dicts. There are 0-2 dicts per bus route, depending on whether INBOUND
+    # and OUTBOUND versions both exist for that route. Each dict has the following structure: {StopIDNumber: [StopName,
+    # Direction, RouteID, RouteName]
+    @staticmethod
+    def get_stop_list():
         url = 'https://truetime.portauthority.org/bustime/wireless/html/home.jsp'
         # Scrape the route names
-        routes = self.scrape_dynamic_tag(url, 'larger')
+        routes = Scraper.scrape_dynamic_tag(url, 'larger')
 
         list_of_dicts = []
 
@@ -127,25 +134,29 @@ class Scraper:
             r_num = r_elements[0].strip()  # The 0-index item in the list is the number
 
             # Check whether inbound and outbound are available for each route
-            outbound, inbound = self.check_available_directions(r_num)
+            outbound, inbound = Scraper.check_available_directions(r_num)
 
             # INBOUND and OUTBOUND are separate HTML pages that must be scraped separately.
             if outbound:
-                url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port+Authority+Bus%3A{r_num}&direction=Port+Authority+Bus%3AOUTBOUND'
-                out_stop_names_and_ids = self.zip_stop_id_and_name(url, 'OUTBOUND', r_num, r_name)
+                url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port' \
+                      f'+Authority+Bus%3A{r_num}&direction=Port+Authority+Bus%3AOUTBOUND'
+                out_stop_names_and_ids = Scraper.zip_stop_id_and_name(url, 'OUTBOUND', r_num, r_name)
                 list_of_dicts.append(out_stop_names_and_ids)
 
             if inbound:
-                url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port+Authority+Bus%3A{r_num}&direction=Port+Authority+Bus%3AINBOUND'
-                in_stop_names_and_ids = self.zip_stop_id_and_name(url, 'INBOUND', r_num, r_name)
+                url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port' \
+                      f'+Authority+Bus%3A{r_num}&direction=Port+Authority+Bus%3AINBOUND'
+                in_stop_names_and_ids = Scraper.zip_stop_id_and_name(url, 'INBOUND', r_num, r_name)
                 list_of_dicts.append(in_stop_names_and_ids)
 
         return list_of_dicts
 
     # For debugging purposes. This function uses bs4 to scrape static webpages for the purpose of learning what tags are
     # available to scrape.
-    def __exploratory_scrape(self):
-        url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port+Authority+Bus%3A1&direction=Port+Authority+Bus%3AOUTBOUND'
+    @staticmethod
+    def __exploratory_scrape():
+        url = f'https://truetime.portauthority.org/bustime/wireless/html/selectstop.jsp?route=Port' \
+              f'+Authority+Bus%3A1&direction=Port+Authority+Bus%3AOUTBOUND'
         page = requests.get(url)
         # Specify lxml parser to avoid different defaults on different machines
         soup = bs4.BeautifulSoup(page.text, features='lxml')
@@ -154,11 +165,12 @@ class Scraper:
         #     print(link.string)
         print(soup.prettify())
 
-    # For debugging purposes. This function tests the output from get_stop_list() to see how many bus lines are associated
-    # with each bus stop.
-    def __test_results(self):
+    # For debugging purposes. This function tests the output from get_stop_list() to see how many bus lines are
+    # associated with each bus stop.
+    @staticmethod
+    def __test_results():
         cnt = 0
-        list_of_dicts = self.get_stop_list()
+        list_of_dicts = Scraper.get_stop_list()
         list_of_ids = []
         for d in list_of_dicts:
             for key in d.keys():
